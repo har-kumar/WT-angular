abbc3516d3f66cfb11cfd6b0a5210770442b0298
HARINI <harinik.22it@kongu..edu>
2024-06-27 09:00:34 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FlightsComponent } from './flights/flights.component';
+import { BookingComponent } from './booking/booking.component';
+import { PaymentComponent } from './payment/payment.component';
+import { CartComponent } from './cart/cart.component';
+
+import { BookingService } from './booking/booking.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'flights', component: FlightsComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FlightsComponent,
+    BookingComponent,
+    PaymentComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [BookingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
